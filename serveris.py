356fda9de87fc1c67817d4866504f29b7c87ce46
Svetlana Zilpausha <Zilpausha@inbox.lv>
2021-01-31 09:44:34 +0200

    pievienots route('/cik/sk1/sk2')

--- serveris.py
+++ serveris.py
@@ -15,10 +15,10 @@ def sveiki():
 @app.route('/sveiki/<vards>')
 def sveikiPersona(vards):
   return f"Sveiki {vards}"
-  """
+  
 @app.route('/cik/<sk1>/<sk2>')
 def reizinajums(sk1, sk2):
   return str(int(sk1)*int(sk2))
-"""
+
 if __name__ == "__main__":
     app.run("0.0.0.0", debug=True)
