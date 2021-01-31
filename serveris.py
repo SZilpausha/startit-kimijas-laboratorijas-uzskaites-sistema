172a4287dbcee69a25463d399af77886e3d499c1
Svetlana Zilpausha <Zilpausha@inbox.lv>
2021-01-31 09:24:11 +0200

    izveidots route(/svek')

--- serveris.py
+++ serveris.py
@@ -10,19 +10,15 @@ def index():
 
 @app.route('/sveiki')
 def sveiki():
-  return "Nav vairs nekāds rīts"
-
+  return "Nav vairs nekāds rīts!"
+"""
 @app.route('/sveiki/<vards>')
 def sveikiPersona(vards):
   return f"Sveiki {vards}"
-
+  
 @app.route('/cik/<sk1>/<sk2>')
 def reizinajums(sk1, sk2):
-  sk1=int(sk1)
-  sk2=int(sk2)
-  reizinajums = sk1 * sk2
-  return str(reizinajums)
-
-
+  return str(int(sk1)*int(sk2))
+"""
 if __name__ == "__main__":
     app.run("0.0.0.0", debug=True)
